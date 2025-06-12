90ee828233b0308496155566a9c565fad6766bb3
Adam Jarvis <jarvisa@algonquincollege.com>
2025-06-12 09:21:44 -0400

    initial files

---
+++ scripts/char.js
@@ -0,0 +1,12 @@
+const charInfo = [
+	[
+		"Compost Heap",
+		"Food Taster",
+		"Compost Heap's strengths are in the creation of delicious and nutritious foodstuffs to heal the group.  Of course, he always keeps a little bit for himself.",
+		"compostHeap.gif",
+		60,
+		80,
+		50
+	],
+	["Felix", "Basket Weaver", "Weaving is a dangerous profession, as Felix knows. Trained from a young age, he can take any wooden object and turn it into a deadly basket.", "felix.gif", 75, 50, 60],["Robopig", "Dentist", "Finding treasure is a lot like pulling teeth, and it turns out that plyers, drills, and mirrors on little sticks are great out on the field.  Robopig is the innovator of the group, and she always has some item for the task at hand.", "robopig.gif", 70, 40, 70],["Slimy", "Proctologist", "Silmy can be a real pain in the nether regions to her enemies, and is most efficient wielding heated iron rods. She is also the brains of the group, easily solving any puzzle.", "slimeMan.gif", 40, 70, 75],["Sock Monster", "Dragon Slayer", "Easily the strongest of the group, Sock Monster can wield any type of weapon, but is most deadly with anything made from cotton.  Dragons beware!", "sockMonster.gif", 90, 30, 75]
+];
